bb5218049a7ba6b709cd2f39aad10debe7dabc31
ph0nk9r <yura.2002.tomen@gmail.com>
2026-07-05 20:04:22 +0300

    Добавлены проверки на наличие обязательных полей и уникальность текста мнения в методах обновления и добавления мнений

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -14,30 +14,42 @@ def get_opinion(id):
 @app.route('/api/opinions/<int:id>/', methods=['PATCH'])
 def update_opinion(id):
     data = request.get_json()
+    if (
+        'text' in data and 
+        Opinion.query.filter_by(text=data['text']).first() is not None
+    ):
+        return jsonify({'error': 
+                        'Такое мнение уже есть в базе данных'}), 400
     opinion = Opinion.query.get_or_404(id)
     opinion.title = data.get('title', opinion.title)
     opinion.text = data.get('text', opinion.text)
     opinion.source = data.get('source', opinion.source)
     opinion.added_by = data.get('added_by', opinion.added_by)
     db.session.commit()  
-    return jsonify({'opinion': opinion.to_dict()}), 200 
+    return jsonify({'opinion': opinion.to_dict()}), 201
 
 @app.route('/api/opinions/<int:id>/', methods=['DELETE'])
 def delete_opinion(id):
     opinion = Opinion.query.get_or_404(id)
     db.session.delete(opinion)
     db.session.commit()
-    return '', 204 
+    return '', 204
 
 @app.route('/api/opinions/', methods=['GET'])
 def get_opinions():
     opinions = Opinion.query.all()
     opinions_list = [opinion.to_dict() for opinion in opinions]
-    return jsonify({'opinions': opinions_list}), 200 
+    return jsonify({'opinions': opinions_list}), 200
 
 @app.route('/api/opinions/', methods=['POST'])
 def add_opinion():
     data = request.get_json()
+    if 'title' not in data or 'text' not in data:
+        return jsonify({'error':
+                        'В запросе отсутствуют обязательные поля'}), 400
+    if Opinion.query.filter_by(text=data['text']).first() is not None:
+        return jsonify({'error': 
+                        'Такое мнение уже есть в базе данных'}), 400
     opinion = Opinion()
     opinion.from_dict(data)
     db.session.add(opinion)
