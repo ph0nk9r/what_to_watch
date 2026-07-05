68758a2c82f29d7f88583978f2ed251aab19c797
ph0nk9r <yura.2002.tomen@gmail.com>
2026-07-05 19:58:02 +0300

    Добавлен API-эндпоинт для получения случайного мнения

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -1,3 +1,5 @@
+from random import randrange
+
 from flask import jsonify, request
 
 from . import app, db
@@ -40,3 +42,11 @@ def add_opinion():
     db.session.add(opinion)
     db.session.commit()
     return jsonify({'opinion': opinion.to_dict()}), 201
+
+@app.route('/api/get-random-opinion/', methods=['GET'])
+def get_random_opinion():
+    quantity = Opinion.query.count()
+    if quantity:
+        offset_value = randrange(quantity)
+        opinion = Opinion.query.offset(offset_value).first()
+        return jsonify({'opinion': opinion.to_dict()}), 200 
