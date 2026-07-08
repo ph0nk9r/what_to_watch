2b2e816e72d7235f6dd7037b5ffd984ab8d924fe
ph0nk9r <yura.2002.tomen@gmail.com>
2026-07-08 09:08:27 +0300

    Перемещен и упрощен код обработки добавления мнений и получения мнений в представлениях

--- opinions_app/views.py
+++ opinions_app/views.py
@@ -29,29 +29,6 @@ def add_opinion_view():
         if Opinion.query.filter_by(text=text).first() is not None:
             flash('Такое мнение уже было оставлено ранее!')
             return render_template('add_opinion.html', form=form)
-        opinion = Opinion(
-            title=form.title.data, 
-            text=text, 
-            source=form.source.data
-        )
-        db.session.add(opinion)
-        db.session.commit()
-        return redirect(url_for('opinion_view', id=opinion.id))
-    return render_template('add_opinion.html', form=form)
-
-@app.route('/opinions/<int:id>')  
-def opinion_view(id):  
-    opinion = Opinion.query.get_or_404(id)
-    return render_template('opinion.html', opinion=opinion) 
-
-@app.route('/add', methods=['GET', 'POST'])
-def add_opinion_view():
-    form = OpinionForm()
-    if form.validate_on_submit():
-        text = form.text.data
-        if Opinion.query.filter_by(text=text).first() is not None:
-            flash('Такое мнение уже было оставлено ранее!')
-            return render_template('add_opinion.html', form=form)
         urls = upload_files_to_dropbox(form.images.data)
         opinion = Opinion(
             title=form.title.data,
@@ -63,3 +40,8 @@ def add_opinion_view():
         db.session.commit()
         return redirect(url_for('opinion_view', id=opinion.id))
     return render_template('add_opinion.html', form=form)
+
+@app.route('/opinions/<int:id>')  
+def opinion_view(id):  
+    opinion = Opinion.query.get_or_404(id)
+    return render_template('opinion.html', opinion=opinion) 
